f578d2f9ea93322437138e4c3334c512d6b6ea35
CalvinBohner <calvin_bohner@asl.org>
2014-12-16 15:08:33 +0000

    Update TestChallenge2.java

--- src/TestChallenge2.java
+++ src/TestChallenge2.java
@@ -1,3 +1,5 @@
+import edu.fcps.karel2.Display;
+
 /**
  * Read and implement all of the TODO items in the main method, below.
  * 
@@ -10,6 +12,18 @@ public class TestChallenge2 {
 		// TODO Create an instance of a Horse inside the pasture
 		// TODO Have the horse wander for 36 steps with a timer of 7
 		// TODO Have the horse explode()
+		
+		
+		Display.openWorld("src/maps/pasture.map");
+        Display.setSize(15, 15);
+        Display.setSpeed(6);
+        
+        Horse jeff = new Horse();
+        jeff.wander(36, 7);
+        
+        
+        
+        
 	}
 
 }
